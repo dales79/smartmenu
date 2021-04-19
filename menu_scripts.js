a0daf58581e5e8473631bc01c932a63f47e0545e
Samantha Eastwood <sam.eastwood.uk@googlemail.com>
2021-04-19 11:27:47 +0100

    testing

--- menu_scripts.js
+++ menu_scripts.js
@@ -14,10 +14,8 @@ $(document).ready(function(){
 	
 	$( '.dropdown' ).click(function(e){
 		if($(this).children('.sub-menu').css('display') == "block") { //its already down and been clicked again
-			//console.log('open');
 			$(this).children('.sub-menu').slideUp(200);
 		} else {
-			//console.log('closed');
 			$('.dropdown').children('.sub-menu').slideUp(200); //slide all of them up		
     		$(this).children('.sub-menu').slideToggle(200);	//and the one we want down		
 		}
